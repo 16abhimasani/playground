e0560e5d0befb7e486129ea494cfa1de98c1dbe9
Ash Bhimasani <ash@bhimasani.com>
2022-11-04 14:00:44 -0400

    query provider

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { AppProvider } from '../contexts/app';
 import { ThemedGlobalStyle } from '../styles';
 import '../styles/styles.scss';
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemedGlobalStyle />
-      <AppProvider>
-        <Component {...pageProps} />
-      </AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <Component {...pageProps} />
+        </AppProvider>
+      </QueryClientProvider>
     </>
   );
 }
