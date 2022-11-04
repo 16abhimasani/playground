6be80a11cdbe574716c336422bf8dcaf34fa2509
Ash Bhimasani <ash@bhimasani.com>
2022-11-04 04:27:52 -0400

    solana env

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,6 +8,60 @@ fn main() {
   println!("\n------RUST-PLAYGROUND------\n");
   let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
   println!("Random f32: {}", rng.gen::<f32>());
+
+  println!("\n------RUST-PLAYGROUND------\n");
+  play_number(Number { value: get_rand_int(6) });
+}
+
+struct Number {
+  value: i32,
+}
+fn play_number(n: Number) {
+  match n.value {
+    1 => println!("Wiggle: {}", n.value),
+    2 => println!("Jiggle: {}", n.value),
+    3 => println!("Belly: {}", n.value),
+    _ => println!("Oh Shucks! {}", n.value),
+  }
+}
+
+fn get_rand_int(range: i32) -> i32 {
+  let mut rng = thread_rng();
+  rng.gen_range(1..=range)
+}
+
+struct Example {
+  number: i32,
+}
+
+impl Example {
+  fn boo() {
+    println!("boo! Example::boo() was called!");
+  }
+
+  fn answer(&mut self) {
+    self.number += 42;
+  }
+
+  fn get_number(&self) -> i32 {
+    self.number
+  }
+}
+
+trait Thingy {
+  // Traits define functionality a particular type has and can share with other types
+  fn do_thingy(&self);
+}
+
+impl Thingy for Example {
+  fn do_thingy(&self) {
+    println!("doing a thing! also, number is {}!", self.number);
+  }
+}
+
+fn thing_returning_closure() -> impl Fn(i32) -> bool {
+  println!("here's a closure for you!");
+  |x: i32| x % 3 == 0
 }
 
 fn play_rand() {
